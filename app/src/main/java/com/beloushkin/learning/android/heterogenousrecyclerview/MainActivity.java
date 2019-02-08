1d35ff7aa81a4ecee4839d83a97205cbeeba4097
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-02-09 00:52:42 +0300

    main menu added

--- app/src/main/java/com/beloushkin/learning/android/heterogenousrecyclerview/MainActivity.java
+++ app/src/main/java/com/beloushkin/learning/android/heterogenousrecyclerview/MainActivity.java
@@ -2,8 +2,21 @@ package com.beloushkin.learning.android.heterogenousrecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toast mToast;
+
+    private void showToast(String msg) {
+        if (mToast != null) {
+            mToast.cancel();
+        }
+        mToast = Toast.makeText(this, msg ,Toast.LENGTH_SHORT);
+        mToast.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +28,26 @@ public class MainActivity extends AppCompatActivity {
                     .beginTransaction()
                     .replace(R.id.container, ListFragment.newInstance())
                     .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_txt_item:
+                showToast(item.getTitle().toString());
+                return true;
+            case R.id.add_img_item:
+                showToast(item.getTitle().toString());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
